a4cd79b75e29936784338673241470a32fd3012d
Dinesh Gamage <dinesh@ecyber.com>
2020-07-07 11:06:41 +0530

    animations added to visitor summary card

--- VisitorSummaryCard/src/index.tsx
+++ VisitorSummaryCard/src/index.tsx
@@ -1,20 +1,76 @@
 import * as React from "react";
 import { registerWidget } from './uxp';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 // styles
 import "./styles.scss";
 
+interface IVisitor {
+    name: string,
+    time: string,
+    index?: number
+}
+
 const VisitorSummaryCardWidget = () => {
+
+    // data set
+    const dataSet: IVisitor[] = [
+        {
+            name: "Mark Anthony",
+            time: "10.00 am",
+        },
+        {
+            name: "Kamal Perere",
+            time: "01.00 pm",
+        },
+        {
+            name: "John Doe",
+            time: "10.00 am",
+        },
+        {
+            name: "Chethiya Perera",
+            time: "10.00 am",
+        }
+    ]
+
+    // state
+    const [visitor, setVisitor] = React.useState<IVisitor | null>({ ...dataSet[0], ...{ index: 0 } })
+
+
+    // toggle prev & next
+    const toggleNext = (current: number) => {
+        let next = (current === (dataSet.length - 1)) ? 0 : (current + 1);
+        console.log(current, " ", next, " ", current === (dataSet.length - 1), " ", dataSet.length)
+        setVisitor({ ...dataSet[next], ...{ index: next } });
+    }
+
+    const togglePrev = (current: number) => {
+        let prev = current == 0 ? (dataSet.length - 1) : (current - 1);
+        setVisitor({ ...dataSet[prev], ...{ index: prev } });
+    }
+
+    // on dismiss 
+    const onDismiss = (current: number) => {
+        toggleNext(current);
+    }
+
+    // on acknowledge
+    const onAcknowledge = (current: number) => {
+        toggleNext(current);
+    }
 
 
     // return 
-    return (<>
-        <div className="visitor-summary-cards ">
+    return (<TransitionGroup className="visitor-summary-card-container">
 
-            <div className="count">12</div>
+        <CSSTransition
+            key={visitor.index}
+            timeout={300}
+            classNames={"fade"}
+        >
+            <div className="visitor-summary-card">
 
-            <div className="visitor-summary-card-content">
-
+                <div className="count">{dataSet.length}</div>
 
                 <div className="header">
                     <div className="title">
@@ -27,32 +83,32 @@ const VisitorSummaryCardWidget = () => {
 
                 <div className="body">
                     <div className="sub">Your visitor is here</div>
-                    <div className="main">Mark Anthony</div>
-                    <div className="sub">for the 10.00 am meeting</div>
+                    <div className="main">{visitor.name}</div>
+                    <div className="sub">for the {visitor.time} meeting</div>
                 </div>
 
                 <div className="footer">
 
                     <div className="controls">
-                        <div className="control-placeholder">
+                        <div className="control-placeholder" onClick={() => togglePrev(visitor.index)}>
                             <div className="control prev"></div>
                         </div>
-                        <div className="control-placeholder">
+                        <div className="control-placeholder" onClick={() => toggleNext(visitor.index)}>
                             <div className="control next active" ></div>
                         </div>
                     </div>
 
                     <div className="action-buttons">
-                        <button className="btn dismiss">dismiss</button>
-                        <button className="btn acknowledge">acknowledge</button>
+                        <button className="btn dismiss" onClick={() => onDismiss(visitor.index)} >dismiss</button>
+                        <button className="btn acknowledge" onClick={() => onAcknowledge(visitor.index)}>acknowledge</button>
                     </div>
 
                 </div>
 
             </div>
-
-        </div>
-    </>);
+        </CSSTransition>
+        <div className="visitor-summary-card-placeholder"></div>
+    </TransitionGroup>);
 }
 
 
@@ -61,6 +117,6 @@ registerWidget({
     "widget": VisitorSummaryCardWidget,
     "configs": {
         "container": { "background": "transparent" },
-        "layout": { w: 5, h: 9, minW: 5, minH: 9}
+        "layout": { w: 5, h: 9, minW: 5, minH: 9 }
     }
 });
